b6c539c0166ce66ef85f30af1b5b97e33606262a
Vasanth V R <vrvasanth1234@gmail.com>
2026-08-09 09:23:55 +0530

    Update transformer.py

--- model/transformer.py
+++ model/transformer.py
@@ -9,7 +9,6 @@ import torch
 import torch.nn as nn
 
 from config import Config
-
 from model.embedding import InputEmbedding
 from model.positional_encoding import PositionalEncoding
 from model.encoder import Encoder
@@ -18,40 +17,18 @@ from model.decoder import Decoder
 
 class Transformer(nn.Module):
 
-    def __init__(self,
-        src_vocab_size,
-        tgt_vocab_size):
-
+    def __init__(self, src_vocab_size, tgt_vocab_size):
         super().__init__()
 
-        # -----------------------------
-        # Source Embedding
-        # -----------------------------
-        self.src_embedding = InputEmbedding(
-            src_vocab_size,
-            Config.D_MODEL
-        )
-
-        # -----------------------------
-        # Target Embedding
-        # -----------------------------
-        self.tgt_embedding = InputEmbedding(
-            tgt_vocab_size,
-            Config.D_MODEL
-        )
-
-        # -----------------------------
-        # Positional Encoding
-        # -----------------------------
+        self.src_embedding = InputEmbedding(src_vocab_size, Config.D_MODEL)
+        self.tgt_embedding = InputEmbedding(tgt_vocab_size, Config.D_MODEL)
+        
         self.positional_encoding = PositionalEncoding(
             d_model=Config.D_MODEL,
             max_seq_len=Config.MAX_SEQ_LEN,
             dropout=Config.DROPOUT
         )
 
-        # -----------------------------
-        # Encoder
-        # -----------------------------
         self.encoder = Encoder(
             num_layers=Config.N_ENCODER_LAYERS,
             d_model=Config.D_MODEL,
@@ -60,9 +37,6 @@ class Transformer(nn.Module):
             dropout=Config.DROPOUT
         )
 
-        # -----------------------------
-        # Decoder
-        # -----------------------------
         self.decoder = Decoder(
             num_layers=Config.N_DECODER_LAYERS,
             d_model=Config.D_MODEL,
@@ -71,79 +45,23 @@ class Transformer(nn.Module):
             dropout=Config.DROPOUT
         )
 
-        # -----------------------------
-        # Final Output Layer
-        # -----------------------------
-        self.output_layer = nn.Linear(
-             Config.D_MODEL,
-            tgt_vocab_size
-        )
-
-    # ----------------------------------------------------
+        self.output_layer = nn.Linear(Config.D_MODEL, tgt_vocab_size)
 
     def encode(self, src, src_mask=None):
-
         src = self.src_embedding(src)
         src = self.positional_encoding(src)
+        return self.encoder(src, src_mask)
 
-        encoder_output = self.encoder(
-            src,
-            src_mask
-        )
-
-        return encoder_output
-
-    # ----------------------------------------------------
-
-    def decode(
-        self,
-        tgt,
-        encoder_output,
-        src_mask=None,
-        tgt_mask=None
-    ):
-
+    def decode(self, tgt, encoder_output, src_mask=None, tgt_mask=None):
         tgt = self.tgt_embedding(tgt)
         tgt = self.positional_encoding(tgt)
-
-        decoder_output = self.decoder(
-            tgt,
-            encoder_output,
-            src_mask,
-            tgt_mask
-        )
-
-        return decoder_output
-
-    # ----------------------------------------------------
+        return self.decoder(tgt, encoder_output, src_mask, tgt_mask)
 
     def project(self, x):
         return self.output_layer(x)
 
-    # ----------------------------------------------------
-
-    def forward(
-        self,
-        src,
-        tgt,
-        src_mask=None,
-        tgt_mask=None
-    ):
-
-        print("Before encode")
-        encoder_output = self.encode(
-            src,
-            src_mask
-        )
-        print("After encode")
-
-        decoder_output = self.decode(
-            tgt,
-            encoder_output,
-            src_mask,
-            tgt_mask
-        )
-
+    def forward(self, src, tgt, src_mask=None, tgt_mask=None):
+        encoder_output = self.encode(src, src_mask)
+        decoder_output = self.decode(tgt, encoder_output, src_mask, tgt_mask)
         logits = self.project(decoder_output)
-
         return logits
