8ccfd71c51e2d2d6913475beb8668e14ef8e1740
Vasanth V R <vrvasanth1234@gmail.com>
2026-08-09 09:24:08 +0530

    Update encoder.py

--- model/encoder.py
+++ model/encoder.py
@@ -14,96 +14,36 @@ from model.residual_connection import ResidualConnection
 
 class EncoderLayer(nn.Module):
 
-    def __init__(
-        self,
-        d_model,
-        num_heads,
-        d_ff,
-        dropout
-    ):
+    def __init__(self, d_model, num_heads, d_ff, dropout):
         super().__init__()
+        self.self_attention = MultiHeadAttention(d_model, num_heads, dropout)
+        self.feed_forward = FeedForward(d_model, d_ff, dropout)
+        
+        self.residual_connections = nn.ModuleList([
+            ResidualConnection(d_model, dropout),
+            ResidualConnection(d_model, dropout)
+        ])
 
-        self.self_attention = MultiHeadAttention(
-            d_model,
-            num_heads,
-            dropout
-        )
-
-        self.feed_forward = FeedForward(
-            d_model,
-            d_ff,
-            dropout
-        )
-
-        self.residual_connections = nn.ModuleList(
-            [
-                ResidualConnection(d_model, dropout),
-                ResidualConnection(d_model, dropout)
-            ]
-        )
-
-    def forward(
-        self,
-        x,
-        src_mask=None
-    ):
-        # Residual Block 1
+    def forward(self, x, src_mask=None):
         x = self.residual_connections[0](
             x,
-            lambda x: self.self_attention(
-                x,
-                x,
-                x,
-                src_mask
-            )[0]
+            lambda x: self.self_attention(x, x, x, src_mask)[0]
         )
-
-        # Residual Block 2
-        x = self.residual_connections[1](
-            x,
-            self.feed_forward
-        )
-
+        x = self.residual_connections[1](x, self.feed_forward)
         return x
 
 
 class Encoder(nn.Module):
 
-    def __init__(
-        self,
-        num_layers,
-        d_model,
-        num_heads,
-        d_ff,
-        dropout
-    ):
+    def __init__(self, num_layers, d_model, num_heads, d_ff, dropout):
         super().__init__()
-
-        self.layers = nn.ModuleList(
-            [
-                EncoderLayer(
-                    d_model,
-                    num_heads,
-                    d_ff,
-                    dropout
-                )
-                for _ in range(num_layers)
-            ]
-        )
-
+        self.layers = nn.ModuleList([
+            EncoderLayer(d_model, num_heads, d_ff, dropout)
+            for _ in range(num_layers)
+        ])
         self.norm = LayerNormalization(d_model)
 
-    def forward(
-        self,
-        x,
-        src_mask=None
-    ):
-        for i, layer in enumerate(self.layers):
-            print(f"Encoder Layer {i} Start")
-            x = layer(
-                x,
-                src_mask
-            )
-            print(f"Encoder Layer {i} End")
-
+    def forward(self, x, src_mask=None):
+        for layer in self.layers:
+            x = layer(x, src_mask)
         return self.norm(x)
