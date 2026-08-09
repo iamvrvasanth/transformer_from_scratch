0467c1da5f510e6c26c47dc964e5a66d077a0902
Vasanth V R <vrvasanth1234@gmail.com>
2026-08-09 09:08:44 +0530

    Update masks.py

--- model/masks.py
+++ model/masks.py
@@ -8,77 +8,40 @@ import torch
 
 
 def create_padding_mask(seq, pad_idx=0):
-    """
-    Creates padding mask.
-
-    Input:
-        Dataset : [seq_len]
-        Training: [batch_size, seq_len]
-
-    Output:
-        [batch_size, 1, 1, seq_len]
-    """
-
-    # Convert single sequence -> batch
-    if seq.dim() == 1:
-        seq = seq.unsqueeze(0)
-
-    mask = (seq != pad_idx).unsqueeze(1).unsqueeze(2)
-
+    mask = (seq != pad_idx)
+    
+    if mask.dim() == 1:
+        # Called from dataset.py: [L] -> [1, 1, L]
+        # DataLoader will stack this to [B, 1, 1, L]
+        return mask.unsqueeze(0).unsqueeze(0)
+        
+    elif mask.dim() == 2:
+        # If called directly on a batch in train.py: [B, L] -> [B, 1, 1, L]
+        return mask.unsqueeze(1).unsqueeze(2)
+        
     return mask.bool()
 
 
 def create_look_ahead_mask(size):
-    """
-    Creates causal (look-ahead) mask.
-
-    Output:
-        [1, 1, size, size]
-    """
-
     mask = torch.tril(
-        torch.ones(
-            (size, size),
-            dtype=torch.bool
-        )
+        torch.ones((size, size), dtype=torch.bool)
     )
-
-    return mask.unsqueeze(0).unsqueeze(0)
+    
+    # Returns [1, L, L]. DataLoader will stack to [B, 1, L, L]
+    return mask.unsqueeze(0)
 
 
 def create_target_mask(tgt, pad_idx=0):
-    """
-    Creates decoder mask.
-
-    Combines:
-        Padding Mask
-            AND
-        Look Ahead Mask
-
-    Input:
-        tgt -> [batch_size, seq_len]
-            or
-        tgt -> [seq_len]
-
-    Output:
-        [batch_size, 1, seq_len, seq_len]
-    """
-
-    # Handle single sequence
-    if tgt.dim() == 1:
-        tgt = tgt.unsqueeze(0)
-
-    padding_mask = create_padding_mask(
-        tgt,
-        pad_idx
-    )
-
-    seq_len = tgt.size(1)
-
-    look_ahead = create_look_ahead_mask(
-        seq_len
-    ).to(tgt.device)
-
+    # Padding mask is [1, 1, L]
+    padding_mask = create_padding_mask(tgt, pad_idx)
+    
+    seq_len = tgt.size(-1)
+    
+    # Look ahead is [1, L, L]
+    look_ahead = create_look_ahead_mask(seq_len).to(tgt.device)
+    
+    # Broadcasting: [1, 1, L] & [1, L, L] -> [1, L, L]
+    # DataLoader will stack this to [B, 1, L, L]
     decoder_mask = padding_mask & look_ahead
-
+    
     return decoder_mask
